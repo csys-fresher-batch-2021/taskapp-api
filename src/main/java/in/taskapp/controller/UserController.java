a98ee01e9f5e35f1a4179ec713477109a81a7b33
Naresh Kumar H <naresh.20210413@gmail.com>
2021-06-17 10:51:58 +0530

    Upgraded - taskcore v1.0.1

--- src/main/java/in/taskapp/controller/UserController.java
+++ src/main/java/in/taskapp/controller/UserController.java
@@ -2,16 +2,18 @@ package in.taskapp.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import in.taskapp.dto.Message;
 import in.taskapp.model.User;
 import in.taskapp.service.UserService;
 
-@Controller
+@CrossOrigin
+@RestController
 @RequestMapping("api/v1/users")
 public class UserController {
 
